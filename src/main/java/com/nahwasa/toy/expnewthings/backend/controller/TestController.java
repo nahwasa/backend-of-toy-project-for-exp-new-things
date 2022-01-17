deb7e385082845c0ca01070a2b9ed236f980efc7
nahwasa-local <nahwasa@gmail.com>
2022-01-17 16:24:10 +0900

    mailing code

--- src/main/java/com/nahwasa/toy/expnewthings/backend/controller/TestController.java
+++ src/main/java/com/nahwasa/toy/expnewthings/backend/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.nahwasa.toy.expnewthings.backend.controller;
 
+import io.swagger.models.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("test")
 @ApiIgnore
 public class TestController {
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @GetMapping
     public String test() {
@@ -19,5 +28,22 @@ public class TestController {
     @GetMapping("/path/{var}/end")
     public String testWithPath(@PathVariable String var) {
         return "Test Path : " + var + " Success";
+    }
+
+    @GetMapping("/sendMail")
+    public ResponseEntity<String> sendMail() {
+        ArrayList<String> mailList = new ArrayList<>();
+        mailList.add("nahwasa@entropykorea.com");
+        mailList.add("jungwooshim@entropykorea.com");
+        mailList.add("hjpark@entropykorea.com");
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo((String[]) mailList.toArray(new String[mailList.size()]));
+        msg.setSubject("여보세요");
+        msg.setText("메일링 테스트");
+
+        javaMailSender.send(msg);
+
+        return ResponseEntity.ok().build();
     }
 }
